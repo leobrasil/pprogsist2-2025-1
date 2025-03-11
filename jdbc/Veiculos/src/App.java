e1c78a4f22c2234330e87f90a5731eb0a0bb9f32
leobrasil <leobrasil@gmail.com>
2025-03-11 23:47:33 +0000

    jdbc

--- jdbc/Veiculos/src/App.java
+++ jdbc/Veiculos/src/App.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
@@ -13,13 +14,24 @@ public class App {
 
             Statement stmt = con.createStatement();
 
+            //Consultando
             ResultSet rs = stmt.executeQuery("SELECT * FROM PROPRIETARIOS");
 
             while(rs.next()){
                 System.out.println(rs.getString("ID"));
             }
 
+            //inserindo
+            String sql_insert = "INSERT INTO PROPRIETARIOS (NOME, CPF) VALUES(?,?)";
 
+            PreparedStatement pstmt = con.prepareStatement(sql_insert);
+
+            pstmt.setString(1,  "antonio");
+            pstmt.setString(2,"333333333-33");
+
+            int qte = pstmt.executeUpdate();
+            if(qte >=1)
+                System.out.println("inserido com sucesso");
 
     }
 }
